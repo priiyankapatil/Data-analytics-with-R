6f805e3ba91e7345cbbbdf87c9a82a6772506fbd
priiyankapatil <priyankaapatil1623@gmail.com>
2024-09-09 02:52:48 -0700

    Add files via upload

---
+++ 30-8-24.R
@@ -0,0 +1,17 @@
+print("Priyanka")
+print("1AY23AI087")
+print("cgpa:8.95")
+#this ia a program
+a<-4#a and b are identifiers
+10->>b
+c=a+b
+c
+"This a string" #string
+help(reserved)
+x=7L
+class(x)
+typeof(x)
+class(a)
+d="hello" #this is a character
+class(d)
+typeof(d)
